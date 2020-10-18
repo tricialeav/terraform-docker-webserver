a66a014a29d1749dc9a57de18d98743051245ddb
Tricia Leavitt <leavitt.tricia@gmail.com>
2020-10-17 21:39:37 -0600

    removing backend config file

--- project/modules/backend-config-dev.tf
+++
@@ -1,5 +0,0 @@
-bucket         = "terraform-docker-webserver-dev-terraform-tf-state"
-key            = "deployments/environments/dev/project/terraform.tfstate"
-region         = "us-west-2"
-encrypt        = true
-dynamodb_table = "terraform-docker-webserver-dev-terraform-tf-state-lock"
