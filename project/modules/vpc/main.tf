00cacde3e9ec22ed051da5b4b915137d95dacbeb
Tricia Leavitt <leavitt.tricia@gmail.com>
2020-10-18 13:00:16 -0600

    Added subnets and updated pipeline

--- project/modules/vpc/main.tf
+++ project/modules/vpc/main.tf
@@ -4,3 +4,27 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = var.enable_dns_hostnames
   tags                 = var.tags
 }
+
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+  tags = var.tags
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = var.tags
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  tags = var.tags
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  tags = var.tags
+}
