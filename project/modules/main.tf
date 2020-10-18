0fb5accb1ed4a58b0999831c7d308765f71e095e
Tricia Leavitt <leavitt.tricia@gmail.com>
2020-10-18 17:22:11 -0600

    bug fix

--- project/modules/main.tf
+++ project/modules/main.tf
@@ -14,6 +14,13 @@ provider "aws" {
   region = var.region
 }
 
+locals {
+  tags = {
+    name = var.prefix
+    env  = var.env
+  }
+}
+
 module "vpc" {
   count                = var.enabled ? 1 : 0
   source               = "./vpc"
@@ -22,14 +29,11 @@ module "vpc" {
   enable_dns_hostnames = true
   availability_zones   = ["us-west-2a", "us-west-2b"]
   subnets              = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  tags = {
-    name = var.prefix
-    env  = var.env
-  }
+  tags = local.tags
 }
 
 module "nat_gateway" {
   count  = var.enabled ? 1 : 0
   source = "./nat_gateway"
-
+  tags = local.tags
 }
