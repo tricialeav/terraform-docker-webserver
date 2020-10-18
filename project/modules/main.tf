d7e63749be82923bd9c689973e53487d8182165f
Tricia Leavitt <leavitt.tricia@gmail.com>
2020-10-18 13:56:54 -0600

    route table test

--- project/modules/main.tf
+++ project/modules/main.tf
@@ -15,11 +15,13 @@ provider "aws" {
 }
 
 module "vpc" {
-  count              = var.enabled ? 1 : 0
-  source             = "./vpc"
-  cidr_block         = "10.0.0.0/16"
-  availability_zones = ["us-west-2a", "us-west-2b"]
-  subnets            = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  count                = var.enabled ? 1 : 0
+  source               = "./vpc"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  availability_zones   = ["us-west-2a", "us-west-2b"]
+  subnets              = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   tags = {
     name = var.prefix
     env  = var.env
--- project/modules/vpc/main.tf
+++ project/modules/vpc/main.tf
@@ -6,9 +6,32 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "subnet" {
-  count             = length(var.subnets)
-  vpc_id            = aws_vpc.vpc.id
-  availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
-  cidr_block        = var.subnets[count.index]
-  tags              = var.tags
+  count                   = length(var.subnets)
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
+  map_public_ip_on_launch = count.index % length(var.availability_zones) == 0 ? true : false
+  cidr_block              = var.subnets[count.index]
+  tags                    = var.tags
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = var.tags
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = var.tags
+}
+
+resource "aws_route_table_association" "public_subnet" {
+  count = length(var.subnets)
+  subnet_id      = count.index % length(var.availability_zones) == 0 ? aws_subnet.subnet[count.index].id : null
+  route_table_id = aws_route_table.public_rt.id
 }
