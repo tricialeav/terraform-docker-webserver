0ce1c026a37aa75fa3522a40ff14a7bbc2e742a5
Tricia Leavitt <leavitt.tricia@gmail.com>
2020-10-18 18:50:06 -0600

    nat gateway fix

--- project/modules/vpc/main.tf
+++ project/modules/vpc/main.tf
@@ -58,11 +58,12 @@ resource "aws_nat_gateway" "gw" {
 }
 
 resource "aws_route_table" "private_rt" {
+  count = length(aws_nat_gateway.gw.id)
   vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.gw.id
+    nat_gateway_id = aws_nat_gateway.gw[count.index].id
   }
 
   tags = var.tags
