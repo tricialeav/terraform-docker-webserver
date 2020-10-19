c3807526b97395d45265bf04cf3dd2653ceaa966
Tricia Leavitt <leavitt.tricia@gmail.com>
2020-10-18 18:19:01 -0600

    output update

--- project/modules/vpc/outputs.tf
+++ project/modules/vpc/outputs.tf
@@ -5,10 +5,10 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   description = "The IDs of the public subnets."
-  value = aws_subnet.public_subnets.id
+  value = aws_subnet.public_subnets.*.id
 }
 
 output "private_subnet_ids" {
   description = "The IDs of the private subnets."
-  value = aws_subnet.private_subnets.id
+  value = aws_subnet.private_subnets.*.id
 }
