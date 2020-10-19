d0220568ac418272f578a7290faf481d98ba6959
Tricia Leavitt <leavitt.tricia@gmail.com>
2020-10-18 18:46:27 -0600

    added private rt

--- project/modules/vpc/main.tf
+++ project/modules/vpc/main.tf
@@ -56,3 +56,20 @@ resource "aws_nat_gateway" "gw" {
   subnet_id     = aws_subnet.public_subnets[count.index].id
   tags          = var.tags
 }
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw.id
+  }
+
+  tags = var.tags
+}
+
+resource "aws_route_table_association" "public_subnet_rt" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
