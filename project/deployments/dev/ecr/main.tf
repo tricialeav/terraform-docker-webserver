0e4fc29d1db6f7c1e627947ba46875cf9f2c5a63
Tricia Leavitt <leavitt.tricia@gmail.com>
2020-10-22 18:13:57 -0600

    update to ecr repos

--- project/deployments/dev/ecr/main.tf
+++ project/deployments/dev/ecr/main.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 resource "aws_ecr_repository" "ecr" {
-  name                 = "web-app"
+  name                 = "web-app-dev"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
--- project/deployments/prod/ecr/main.tf
+++ project/deployments/prod/ecr/main.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 resource "aws_ecr_repository" "ecr" {
-  name                 = "web-app"
+  name                 = "web-app-prod"
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
